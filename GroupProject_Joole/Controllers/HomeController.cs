0af4494b9389fae34537cd1d07a7f9b87b786d9d
Dake Shen <brianshen0216@hotmail.com>
2023-01-31 14:20:13 -0500

    Add project files.

---
+++ GroupProject_Joole/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject_Joole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult Summary()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Compare()
+        {
+            return View();
+        }
+    }
+
+}
