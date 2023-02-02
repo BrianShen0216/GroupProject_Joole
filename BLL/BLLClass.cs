4183d4ad612631725c8d5608c33b9caaad888906
CharlesK <xk2144@columbia.edu>
2023-02-02 00:23:27 -0500

    finish main page and filter skeleton

--- BLL/BLLClass.cs
+++ BLL/BLLClass.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,129 @@ namespace BLL
 {
     public class BLLClass
     {
+        private DALClass _dal;
+        public BLLClass()
+        {
+            _dal = new DALClass();
+        }
+
+        public List<ProductDetail> GetAllProdcutDetails()
+        {
+            return _dal.GetAllProdcutDetails();
+        }
+
+        public List<Products> GetAllProducts()
+        {
+            return _dal.GetAllProducts();
+        }
+
+        public List<Products> GetSubCategoryProducts(int subCategoryID)
+        {
+            return _dal.GetSubCategoryProducts(subCategoryID);
+        }
+
+        public List<Products> FilterProducts(List<Products> products, Filters filters)
+        {
+            List<Products> result = products;
+            foreach(var tf in filters.TypeFilters)
+            {
+                result = result.Where(p =>
+                {
+                    foreach(var pv in p.PropertyValue)
+                    {
+                        if (pv.PropertyID==tf.PropertyID && pv.Property.IsType == true && pv.PropertyValue1==tf.Value)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }).ToList();
+            }
+            foreach(var tsf in filters.TechSpecFilters)
+            {
+                result = result.Where(p =>
+                {
+                    foreach (var pv in p.PropertyValue)
+                    {
+                        if (pv.Property.IsTechSpec == true && pv.PropertyID == tsf.PropertyID)
+                        {
+                            try
+                            {
+                                int val = Int32.Parse(pv.PropertyValue1);
+                                if (val>=tsf.MinValue && val<=tsf.Value)
+                                {
+                                    return true;
+                                }
+                            } catch (FormatException ex)
+                            {}
+                        }
+                    }
+                    return false;
+                }).ToList();
+            }
+            return result;
+        }
+
+        public List<TypeFilterWithValue> GetTypeFilters(int subCategoryID)
+        {
+            SubCategory sub = _dal.GetSubCategory(subCategoryID);
+            if (sub==null)
+            {
+                return new List<TypeFilterWithValue>();
+            }
+            return sub.TypeFilter.Select(tf => new TypeFilterWithValue
+            {
+                PropertyID = tf.PropertyID,
+                SubCategoryID = tf.SubCategoryID,
+                TypeName = tf.TypeName,
+                PropertyName = tf.Property.PropertyName
+            }).ToList();
+        }
+
+        public List<TechSpecFilterWithValue> GetTechSpecFilters(int subCategoryID)
+        {
+            SubCategory sub = _dal.GetSubCategory(subCategoryID);
+            if (sub == null)
+            {
+                return new List<TechSpecFilterWithValue>();
+            }
+            return sub.TechSpecFilter.Select(tsf => new TechSpecFilterWithValue
+            {
+                PropertyID = tsf.PropertyID,
+                SubCategoryID = tsf.SubCategoryID,
+                MaxValue = tsf.MaxValue,
+                MinValue= tsf.MinValue,
+                PropertyName = tsf.Property.PropertyName
+            }).ToList();
+        }
+
+        public Filters GetFilters(int subCategoryID)
+        {
+            Filters filters = new Filters();
+            SubCategory sub = _dal.GetSubCategory(subCategoryID);
+            if (sub == null)
+            {
+                filters.TypeFilters = new List<TypeFilterWithValue>();
+                filters.TechSpecFilters = new List<TechSpecFilterWithValue>();
+            }else
+            {
+                filters.TypeFilters = sub.TypeFilter.Select(tf => new TypeFilterWithValue
+                {
+                    PropertyID = tf.PropertyID,
+                    SubCategoryID = tf.SubCategoryID,
+                    TypeName = tf.TypeName,
+                    PropertyName = tf.Property.PropertyName
+                }).ToList();
+                filters.TechSpecFilters = sub.TechSpecFilter.Select(tsf => new TechSpecFilterWithValue
+                {
+                    PropertyID = tsf.PropertyID,
+                    SubCategoryID = tsf.SubCategoryID,
+                    MaxValue = tsf.MaxValue,
+                    MinValue = tsf.MinValue,
+                    PropertyName = tsf.Property.PropertyName
+                }).ToList();
+            }
+            return filters;
+        }
     }
 }
--- GroupProject_Joole/Controllers/HomeController.cs
+++ GroupProject_Joole/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,12 @@ namespace GroupProject_Joole.Controllers
 {
     public class HomeController : Controller
     {
+        private BLLClass _bll;
+        public HomeController() : base()
+        {
+            _bll= new BLLClass();
+        }
+
         public ActionResult Login()
         {
             return View();
@@ -20,7 +28,42 @@ namespace GroupProject_Joole.Controllers
 
         public ActionResult Search()
         {
-            return View();
+            //List<ProductDetail> rawData = new List<ProductDetail>
+            //{
+            //    new ProductDetail
+            //    {
+            //        ProductID= 1,
+            //        Manufacturer = new Manufacturers
+            //        {
+            //            ManufacturerID= 1,
+            //            ManufacturerName="Amazon",
+            //            ManufacturerWeb="amazon.com"
+            //        },
+            //        SubCategoryID= 1,
+            //        ProductName="FanA",
+            //        ProductImage=null,
+            //        Series="Series350",
+            //        Model="A",
+            //        ModelYear=2013,
+            //        PropertyID= 1,
+            //        PropertyName="AirFlow (CFM)",
+            //        PropertyValue1="68",
+            //        IsType=false,
+            //        IsTechSpec=true
+            //    }
+            //};
+            List<Products> products = _bll.GetSubCategoryProducts(1);
+            Filters filters = _bll.GetFilters(1);
+            TempData["Products"] = products;
+            TempData["Filters"] = filters;
+            return View("MainPage");
+        }
+        [HttpGet]
+        public ActionResult ApplyFilter(Filters filters)
+        {
+            List<Products> products = (List<Products>)TempData.Peek("Products");
+            List<Products> filteredProducts = _bll.FilterProducts(products, filters);
+            return PartialView("ProductDisplay", filteredProducts);
         }
         public ActionResult Summary()
         {
