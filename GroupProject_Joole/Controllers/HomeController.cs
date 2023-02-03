1992a369157b6b9e41c34caf3dfbe8f30a9f10dc
LynnLi26 <lilynn542@gmail.com>
2023-02-03 17:06:21 -0500

    Login and siguup

--- GroupProject_Joole/Controllers/HomeController.cs
+++ GroupProject_Joole/Controllers/HomeController.cs
@@ -1,22 +1,56 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DAL.Models;
+using System.Web.UI.WebControls;
+using GroupProject_Joole.Models;
+
 
 namespace GroupProject_Joole.Controllers
 {
     public class HomeController : Controller
     {
+        protected JooleDatabaseEntities userEntity = new JooleDatabaseEntities();
         public ActionResult Login()
         {
-            return View();
+            /*JooleDatabaseEntities entities = new JooleDatabaseEntities();
+            var ls = entities.Users.ToList();*/
+            return View("Login");
         }
 
         public ActionResult SignUp()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SignUp(Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("GetInfor",users);
+            }
+            return View();
+        }
+
+        public ActionResult GetInfor(Users users)
+        //public ActionResult GetInfor(ModelUser ojbUser)
+        {
+            //userEntity.addUser(ojbUser.UserName, ojbUser.UserEmail, ojbUser.UserPassword);
+
+
+            var list = userEntity.Users.ToList();
+            return View(list);                
+        }
+
+        /*public ActionResult SignUp()
+        {
+
+            return View();
+        }*/
 
         public ActionResult Search()
         {
---
+++ GroupProject_Joole/Models/ModelUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject_Joole.Models
+{
+    public class ModelUser
+    {
+        public int UserID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match")]
+        public string UserConPassword { get; set; }
+    }
+}
